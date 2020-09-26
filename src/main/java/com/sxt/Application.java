73d501487c5ec46e6e99fd7c07e525830bd06243
randy1130 <randylee1130@163.com>
2020-09-26 23:41:21 +0800

    测试修改

--- src/main/java/com/sxt/Application.java
+++ src/main/java/com/sxt/Application.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages= {"com.sxt.*.mapper"})
+@MapperScan(basePackages = {"com.sxt.*.mapper"})
 public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("123");
+    }
 }
